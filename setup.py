cfbd14356511c8d7817750c22acf3164ff0030de
Konstantin Enchant <sirkonst@gmail.com>
2014-10-02 22:56:58 +0400

    Bump 1.0.1

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from distutils.core import setup
 
 setup(
     name="Timecard",
-    version="1.0",
+    version="1.0.1",
     packages=["timecard"],
     url="http://github.com/selectel/timecard",
     license="MIT",
